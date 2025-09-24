af483e1acf0ce0b52c728dc3aa2fa5a3c5f21721
stanislav <stanislav.vasilchenko@yandex.ru>
2025-09-24 21:09:11 +0300

    [+] Сервисные функции для преобразованья исходного текста

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher
 
 from config.config import load_config
 from database.database import db_init
+from services.file_handling import prepare_book
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +20,10 @@ async def main():
 
     bot = Bot(token=config.bot.token)
     dp = Dispatcher()
+
+    logger.info("Preparing book")
+    book = prepare_book("book/book.txt")
+    logger.info("The book is uploaded. Total pages: %d", len(book))
 
     db: dict = db_init()
 
